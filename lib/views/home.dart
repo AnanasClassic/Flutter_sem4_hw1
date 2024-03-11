3b166424e0825de9568b3b7d3c4f3234834272a7
AnanasClassic <kuznetsov.vp@phystech.edu>
2024-03-11 23:43:28 +0300

    fix bug with white background in dark mode

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -61,6 +61,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _darkMode ? Colors.black : Colors.white,
         appBar: AppBar(
             backgroundColor: _darkMode ? Colors.black : Colors.white,
             title: Row(
